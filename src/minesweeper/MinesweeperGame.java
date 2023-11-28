e4c9a2882a5af0480f7fc0830cfc43878f675512
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-11-28 20:13:15 +0600

    Update MinesweeperGame.java to resolve AudioClip deprecation warnings

--- src/minesweeper/MinesweeperGame.java
+++ src/minesweeper/MinesweeperGame.java
@@ -1,8 +1,16 @@
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 import javax.swing.*;
+import java.io.File;
+
+
+
 
 public class MinesweeperGame extends JFrame {
     private JButton[][] buttons;
@@ -18,6 +26,10 @@ public class MinesweeperGame extends JFrame {
     // Timer variables
     private Timer timer;
     private int secondsPlayed;
+
+    // Sound effect for exploding mines
+    private final String EXPLOSION_SOUND_PATH = "../resources/explosion_sound.wav";
+    private Clip explosionSound;
 
     public MinesweeperGame() {
         setTitle("Minesweeper");
@@ -48,6 +60,15 @@ public class MinesweeperGame extends JFrame {
 
         // Start the timer
         timer.start();
+
+         // Load the audio clip
+         try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(EXPLOSION_SOUND_PATH));
+            explosionSound = AudioSystem.getClip();
+            explosionSound.open(audioInputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         pack();
         setVisible(true);
@@ -89,7 +110,60 @@ public class MinesweeperGame extends JFrame {
     }
 
     private void uncoverCell(int i, int j) {
-        // ... (rest of the code)
+        if (!buttons[i][j].isEnabled()) {
+            return;  // cell already uncovered or flagged
+        }
+
+        buttons[i][j].setEnabled(false);
+        uncoveredCells++;
+
+        if (mines[i][j]) {
+            explodeMine(i, j);
+        } else {
+            if (uncoveredCells == ROWS * COLS - MINES) {
+                endGame(true);  // All non-mine cells uncovered, player wins
+            } else if (surroundingMines[i][j] == 0) {
+                // Auto-uncover adjacent cells if the current cell has no surrounding mines
+                for (int x = Math.max(0, i - 1); x <= Math.min(ROWS - 1, i + 1); x++) {
+                    for (int y = Math.max(0, j - 1); y <= Math.min(COLS - 1, j + 1); y++) {
+                        uncoverCell(x, y);
+                    }
+                }
+            } else {
+                buttons[i][j].setText(Integer.toString(surroundingMines[i][j]));
+            }
+        }
+    }
+
+    private void explodeMine(int i, int j) {
+        // Play explosion sound
+        if (explosionSound != null) {
+            explosionSound.start();
+        }
+
+        // Display explosion animation (you can add your animation logic here)
+
+        // Mark exploded mine with a special symbol
+        buttons[i][j].setText("*");
+
+        // Disable all buttons
+        disableAllButtons();
+
+        // End the game
+        endGame(false);
+    }
+
+    private void disableAllButtons() {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                buttons[i][j].setEnabled(false);
+            }
+        }
+    }
+
+    private void endGame(boolean win) {
+        // ... (code to handle game ending, display messages, etc.)
+        // You can customize this method based on your needs.
     }
 
     private class CellClickListener implements ActionListener {
@@ -106,23 +180,14 @@ public class MinesweeperGame extends JFrame {
         }
     }
 
-    // Timer listener
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             secondsPlayed++;
 
-            // Check if the timer exceeds 60 seconds (1 minute) for the Beginner level
             if (secondsPlayed >= 60) {
                 timer.stop();
-                endGame(false); 
+                endGame(false);
             }
         }
     }
-
-    // Method to end the game
-    private void endGame(boolean win) {
-        // ... (code to handle game ending, display messages, etc.)
-    }
-
-    // ... (rest of the code)
 }
